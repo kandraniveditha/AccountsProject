f640d3ec3bf3c295f4c3c4866b34d64073c689d9
niveditha <kandraniveditha19@gmail.com>
2024-05-03 18:45:46 +0530

    Working on Accounts Projects for Pratice

---
+++ src/main/java/com/eazybytes/accounts/constants/AccountsConstants.java
@@ -0,0 +1,16 @@
+package com.eazybytes.accounts.constants;
+
+public class AccountsConstants {
+
+    private AccountsConstants() {
+    }
+        public static final String SAVINGS="Savings";
+        public static final String ADDRESS="#15 ,Bangalore,Karnataka";
+        public static final String STATUS_201="201";
+        public static final String MESSAGE_201="Account created successfully";
+        public static final String STATUS_200="200";
+        public static final String MESSAGE_200="Request Processed successfully";
+        public static final String STATUS_500="500";
+        public static final String MESSAGE_500="An error occurred please try again or contact Dev team";
+
+}
---
+++ src/main/java/com/eazybytes/accounts/service/IAccountsService.java
@@ -0,0 +1,12 @@
+package com.eazybytes.accounts.service;
+
+import com.eazybytes.accounts.dto.CustomerDto;
+
+public interface IAccountsService {
+
+    /**
+     *
+     * @param customerDto-customerDto object
+     */
+    void createAccount(CustomerDto customerDto);
+}
